bbb0812070c459f84d9f3d9081a641038ae9d43c
stas <zstanislaw211@gmail.com>
2018-12-23 01:30:16 +0300

    begining

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+/*
+cd c:/projects
+cd angular
+cd SportsStore
+ng serve --port 3000 --open
+*/
